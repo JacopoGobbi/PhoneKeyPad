b55b6db815aca5608ebe9b26a981f449fedfe499
JacopoGobbi <jacopogobbi94@gmail.com>
2019-02-27 03:46:24 +0100

    Upgrades and scalatest plus play

--- build.sbt
+++ build.sbt
@@ -9,7 +9,7 @@ scalaVersion := "2.12.8"
 
 libraryDependencies ++= Seq(
   guice,
-  "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % Test
+  "org.scalatestplus.play" %% "scalatestplus-play" % "4.0.1" % Test
 )
 
 // Adds additional packages into Twirl
--- test/controllers/HomeControllerSpec.scala
+++ test/controllers/HomeControllerSpec.scala
@@ -14,7 +14,6 @@ import play.api.test.Helpers._
 class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting {
 
   "HomeController GET" should {
-
     "render the index page from a new instance of controller" in {
       val controller = new HomeController(stubControllerComponents())
       val home = controller.index().apply(FakeRequest(GET, "/"))
